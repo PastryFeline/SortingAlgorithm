d8eabe259f245a8079c04488e07fa4caae7015fd
Rasmus Mikkel Cæsarsen <rasm639q@elev.techcollege.dk>
2022-10-14 07:41:44 +0200

    added comments because that's a thing moodle says i should

--- SortingAlgorithm/Program.cs
+++ SortingAlgorithm/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
+            //list
             List<int> list = new List<int>();
+            //more list
             list.Add(11); list.Add(25); list.Add(3); list.Add(14); list.Add(5); list.Add(26); list.Add(57); list.Add(9); list.Add(15); list.Add(49);
-            InsertionSort(list);
+            //method
+            SortStuff(list);
+            //write list to console
             for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine(list[i]);
@@ -14,20 +18,22 @@
 
         }
 
-        public static void InsertionSort(List<int> input)
+        public static void SortStuff(List<int> input)
         {
-
+            //takes every item in list
             for (int i = 0; i < input.Count(); i++)
             {
                 int item = input[i];
                 int currentIndex = i;
 
+                //checks each item if it has a lower value than the one above and if it is, it moves it up until it's the same value or higher
                 while (currentIndex > 0 && input[currentIndex - 1] > item)
                 {
                     input[currentIndex] = input[currentIndex - 1];
                     currentIndex--;
                 }
 
+                //inserts the item to the correct position
                 input[currentIndex] = item;
             }
         }
